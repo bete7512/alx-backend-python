d80075208202d65cd1a8116348248957f071372f
bete7512 <betekbebe@gmail.com>
2023-01-05 17:57:03 +0300

    one thing

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,4 +4,7 @@
 
 def sum_list(input_list: list[float]) -> float:
     """ Sum a list of floats """
-    return sum(input_list)
+    sum = 0
+    for i in input_list:
+        sum += i
+    return sum
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,5 +4,8 @@
 
 def sum_mixed_list(mxd_lst: list[mixed]) -> float:
     """ Sum a list of floats and ints """
-    return sum(mxd_lst)
+    sum = 0
+    for i in mxd_lst:
+        sum += i
+    return sum
     # return sum(map(lambda x: float(x), mxd_lst))
