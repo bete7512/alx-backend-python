cb917f00673edbd492d03f7f08c6022bf3d86d74
bete7512 <betekbebe@gmail.com>
2023-01-05 18:07:23 +0300

    one more thing about python backend programming

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,9 @@
 """ 0x00. Python - Variable Annotations """
 
 
+from typing import Tuple, Union
+
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Return a tuple """
     return (k, v * v)
