b7e81cac250f9fa338bf85827bf29962b45926f3
bete7512 <betekbebe@gmail.com>
2023-01-05 18:03:53 +0300

    one more thing about python backend programming

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,10 +3,7 @@
 from typing import List
 
 
-def sum_mixed_list(mxd_lst: List[mixed]) -> float:
-    """ Sum a list of floats and ints """
-    sum = 0
-    for i in mxd_lst:
-        sum += i
-    return sum
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ Sum a list of floats and ints"""
+    return sum(mxd_lst)
     # return sum(map(lambda x: float(x), mxd_lst))
