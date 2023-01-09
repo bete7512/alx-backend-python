6d45e30f324fb5a7bee723ede35bd969c36eac56
bete7512 <betekbebe@gmail.com>
2023-01-09 14:52:20 +0300

    asynchronous function using python

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""async python functions"""
+import random
+import asyncio
+
+async def wait_random(max_delay:int =10)->float:
+    """wait random time"""
+    random_float = random.uniform(0, max_delay)
+    await asyncio.sleep(random_float)
+    return random_float
