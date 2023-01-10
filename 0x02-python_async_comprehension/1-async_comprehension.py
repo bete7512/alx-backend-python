7c46af6f753380ac7c89bc70602a9f6c34901668
bete7512 <betekbebe@gmail.com>
2023-01-10 19:39:39 +0300

    one time killer for web3

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 """ async generator """
 import asyncio
-from typing import Generator
-import random
-
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
-    """ async generator """
+async def async_comprehension() -> List[float]:
+    """ Async Comprehensions """
     return [i async for i in async_generator()]
