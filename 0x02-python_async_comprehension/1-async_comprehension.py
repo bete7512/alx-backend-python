aedd82f48a443d455f5d51f415ce981ebce0e472
bete7512 <betekbebe@gmail.com>
2023-01-10 19:37:57 +0300

    one time killer for web3

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,6 +6,7 @@ import random
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> Generator[float, None, None]:
     """ async generator """
     return [i async for i in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,6 +5,7 @@ import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """ Measure runtime """
     start = time.perf_counter()
